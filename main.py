22c33dee86bdc93e6de09137f31aba26217e05f8
BielSousa <36546452+BielSousa@users.noreply.github.com>
2021-03-25 17:11:15 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from gsheet import GSheet
 PLAN = 'https://docs.google.com/spreadsheets/d/e/your_key_sheet/pubhtml'
 
 tabela = GSheet(PLAN)
-tabela.select_table('senhas')
+tabela.select_table('plan1')
 tabela.show_table()
-tabela.save_table('senhas')
+tabela.save_table('my_table')
 tabela.show_table()
-tabela.load_table('senhas')
+tabela.load_table('mytable')
 tabela.show_table()
